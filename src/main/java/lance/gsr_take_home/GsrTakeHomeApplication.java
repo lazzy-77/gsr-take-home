7dae285765e075b503258149375b239c6be1ded5
Lance Nieva <lance.nieva20@gmail.com>
2024-06-13 00:53:14 +0100

    start consumer on separate thread

--- src/main/java/lance/gsr_take_home/GsrTakeHomeApplication.java
+++ src/main/java/lance/gsr_take_home/GsrTakeHomeApplication.java
@@ -1,5 +1,6 @@
 package lance.gsr_take_home;
 
+import lance.gsr_take_home.kafka.MessageConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GsrTakeHomeApplication {
 
 	public static void main(String[] args) {
+		var consumer = new MessageConsumer("kraken");
+		new Thread(consumer::consumeMessages).start();
+
 		SpringApplication.run(GsrTakeHomeApplication.class, args);
 	}
 
